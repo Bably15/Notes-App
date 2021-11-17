38d17f2d55ce46c86693b87c0321994b2f066fd0
Bably <bablyd15@gmail.com>
2021-11-17 23:44:10 +0530

    sent data from form to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
-import Input from "./components/Input";
+import Task from "./components/Task";
 import Footer from "./components/Footer";
 
 function App() {
+    const [taskData, setTaskData] = useState([]);
+    const noteData = (note) => {
+        setTaskData([...taskData, note]);
+        // console.log(note);
+    };
     return (
         <div className="App">
             <Header />
-            <Input />
-            <Form />
-            <Form />
-            <Form />
+            <Form passNoteData={noteData} />
+            <Task />
             <Footer />
         </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
 
-const Form = () => {
+const Form = (props) => {
+    const { passNoteData } = props;
+    const [formData, setFormData] = useState({
+        title: "",
+        content: "",
+    });
+
+    const formChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        passNoteData(formData);
+    };
     return (
-        <Paper sx={{ display: "inline-flex", margin: "20px", padding: "10px" }}>
-            <div>
-                <h1>Title</h1>
-                <p>Content</p>
-            </div>
+        <Paper>
+            <form onSubmit={formSubmitHandler}>
+                <TextField
+                    name="title"
+                    value={formData.title}
+                    id="Title"
+                    label="title"
+                    variant="standard"
+                    onChange={formChange}
+                />
+                <TextField
+                    name="content"
+                    value={formData.content}
+                    onChange={formChange}
+                    id="content"
+                    label="content"
+                    variant="standard"
+                />
+                <Fab color="primary" aria-label="add" type="submit">
+                    <AddIcon />
+                </Fab>
+            </form>
         </Paper>
     );
 };
