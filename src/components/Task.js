eb126142e44b27736f1b5686bab1fc779abdd415
Bably <bablyd15@gmail.com>
2021-11-19 22:32:24 +0530

    aligned items

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,23 @@
 import React from "react";
-import Paper from "@mui/material/Paper";
+import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
 
 const Task = (props) => {
     const { passTaskData } = props;
     return (
         <Grid container spacing={2} sx={{ margin: "30px" }}>
-            <Grid item xs={3}>
-                <Paper elevation={2} sx={{ padding: "50px" }} key="uniqueid1">
-                    {passTaskData.map((task) => (
-                        <div>
-                            <span>{task.title}</span>
-                            {task.content}
-                        </div>
-                    ))}
-                </Paper>
+            <Grid
+                item
+                xs={4}
+                display="inline-flex"
+                justifyContent="space-between"
+            >
+                {passTaskData.map((task) => (
+                    <Card sx={{ padding: "50px" }}>
+                        <div>{task.title}</div>
+                        {task.content}
+                    </Card>
+                ))}
             </Grid>
         </Grid>
     );
